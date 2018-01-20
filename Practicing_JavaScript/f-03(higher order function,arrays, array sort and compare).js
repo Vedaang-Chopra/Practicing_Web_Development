c4e4a8f1077021d2b973961da9b1201a20f33f00
Vedaang Chopra <vedaangchopra@hotmail.com>
2018-01-20 19:07:18 +0530

    Adding JavaScript Practice

--- Practicing_JavaScript/f-03(higher order function,arrays, array sort and compare).js
+++ Practicing_JavaScript/f-03(higher order function,arrays, array sort and compare).js
@@ -10,14 +10,14 @@ function sayhello(hellofn) {                            //This is an example of 
 }
 sayhello(hello);
 /******************************************************************************************************************************/
-let arr1=[11,33,40,90,50]
+let arr1=[117,433,430,390,150,631,531]
 console.log(arr);
 function numIncCompare(a,b) {
      console.log(a);
      console.log(b);                                                             //This here is insertion sort
      console.log((((a%10)-(b%10)))*10);
      console.log((((a/10)%10) -((b/10)%10)));
-     console.log((((a%10)-(b%10))+ ((a/10)%10) -((b/10)%10)));
-     return    (((a%10)-(b%10))*10)+ (((a/10)%10) -((b/10)%10)) ;                                             //This sorting is not language specific
+     console.log((((a%10)-(b%10))+ ((a/10)%10)-((b/10)%10)));                    //This sorting is not language specific
+     return    (((a%10)-(b%10))*100)+ ((((a/10)%10) -((b/10)%10))*10)+((((a/100)%10) -((b/100)%10))) ;
 }
 console.log(arr1.sort(numIncCompare))
