486faaf6e5bb46d27594ba72994186367fd536b0
Vedaang Chopra <vedaangchopra@hotmail.com>
2018-01-23 00:55:12 +0530

    Adding JavaScript Practice

--- Practicing_JavaScript/f-04.js
+++ Practicing_JavaScript/f-04.js
@@ -8,9 +8,10 @@ function creategreeter (name) {
     }
     return greeter;
 }
-let worldgreeter=creategreeter('world');
+let worldgreeter=creategreeter('world');            //Here worldgreeter turns out to be a function
 worldgreeter;
 function fakesum(a,b) {
+   function sum()
     {
         console.log(a+b);
     }
@@ -18,3 +19,27 @@ function fakesum(a,b) {
 }
 var a=fakesum(3,4);
 console.log(a);
+/********************************************************************************************************************************/
+//Functions as objects......................................................................................
+let obj={p:1,q:2,r:3};
+obj.x=40;
+console.log(obj);
+function alpha()
+{
+
+}
+alpha.y=50;
+console.log(alpha);
+console.log(typeof (alpha));
+/**********************************************************************************************************************************/
+//Scopes and Closures..............................................................................................
+//let v=10;
+function beta()
+{
+    v=20;
+    return v;
+}
+console.log(v);                 // Here as v is not declared and strict mode is applicable we cannot use v
+console.log(beta());
+
+
