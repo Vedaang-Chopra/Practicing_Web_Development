0870c51968cb21e3e1e29e0adc5bfadbf6686a38
Vedaang Chopra <vedaangchopra@hotmail.com>
2018-01-23 16:28:05 +0530

    Adding JavaScript Practice

--- Practicing_JavaScript/f-05(closures, reduce map filter, split and join).js
+++ Practicing_JavaScript/f-05(closures, reduce map filter, split and join).js
@@ -13,6 +13,34 @@ console.log(mycounter());
 console.log(mycounter());
 /***********************************************************************************************************************************/
 
+//Array Reduce,Map,Filter.....................................................................................................
+
+let arr=[1,2,3,4,5,6,7,8];
+let sqrr=arr.map(function (item,index,array)
+{                                                  //This  map function is used to access each element of array and used
+    return item*item;                              //to change array's every member value.Thus the function passed into
+                                                   //map having three arguments item index and array refer to each
+});                                                //element's attributes.eg. item=1,index=0,array="arr"
+console.log(sqrr);
+console.log(Math.sqrt(sqrr[4]));
+
+let eqrr=arr.map(function (item,index,array)
+{
+    return array[arr.length-index-1];
+});
+/*let zqrr=arr.map(function (d,i,a) {
+    if(i==0)
+        return d;
+    return a[i-1]+a[i];
+
+})
+console.log("Fibonacci:")
+console.log(zqrr)*/
+console.log(eqrr);
+
+let arr1=[1,2,3,4,5,6,7,8,9,0];
+
+
 
 
 
