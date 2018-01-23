4d965d3685d93c483511ad6967bc0de3d2ed2935
Vedaang Chopra <vedaangchopra@hotmail.com>
2018-01-23 21:07:03 +0530

    Practicing JavaScript

--- Practicing_JavaScript/f-07(this).js
+++ Practicing_JavaScript/f-07(this).js
@@ -3,7 +3,6 @@
 console.log("Using this .........................................................")
 function alpha()
 {
-    //console.log(this);
     this.b=11;
 }
 alpha();
@@ -11,7 +10,21 @@ console.log(b);
 let obj={
     p:10,q:20,
     r:function(){
-        console.log(this);                  //Here this points to the object "obj"
-    }
-}
+        console.log(this);               //Here this points to the object "obj" as r is defined here.
+        alpha();                        //Even though alpha is called her yet b created in alpha would not point to b,
+    }                                   //it would point towards global only as alpha is defined in global. This is called
+}                                       // as lexical scoping, that is where there is definition this would point there only.
+console.log(b);
+
+
+console.log(obj);
 obj.r();
+//Using new constructor...........................................................................
+function newPerson(name,age)
+{
+    this.name=name;
+    this.age=age;
+
+}
+let p=new newPerson("Joe",32);
+console.log(p);
