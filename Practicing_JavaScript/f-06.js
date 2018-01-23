2f3fd7ba4290bad8a8902a938105e5ba96ef05a7
Vedaang Chopra <vedaangchopra@hotmail.com>
2018-01-23 17:45:37 +0530

    Adding JavaScript Practice

--- Practicing_JavaScript/f-06.js
+++ Practicing_JavaScript/f-06.js
@@ -1,11 +1,10 @@
 /**********************************************************************************************************************************/
-//Prototypes...........................................................................................................
 //Creating classes using function and objects..........................................................................
 "use strict";
 function createPerson(name,age)
 {
     return{
-        name:name,
+        name:name,                      //Here a single entity ,an object is being returned.
         age:age,
         isAdult: function()
         {
@@ -14,7 +13,8 @@ function createPerson(name,age)
     }
 
 }
-let p1=createPerson("Joe",32)
+let p1=createPerson("Joe",32)           //Here a fake class or an(object) leads to creation of  another object
 console.log(p1.isAdult());
+//Here p1 is an object of javascript.Thus new data can be added or deleted without change in class
 delete p1.name;
 console.log(p1);
