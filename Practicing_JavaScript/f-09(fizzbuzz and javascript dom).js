9b6282dfaff2eb381df64183dd3f1f5990874db5
Vedaang Chopra <vedaangchopra@hotmail.com>
2018-01-30 19:05:53 +0530

    JavaScript Practice

--- Practicing_JavaScript/f-09(fizzbuzz and javascript dom).js
+++ Practicing_JavaScript/f-09(fizzbuzz and javascript dom).js
@@ -1,29 +1,32 @@
 function fizzbuzzchallenge(){
     let ul=document.getElementById("fizzbuzz");
-    let d;
-
+    let d,g;
+    let e=(new Date().getTime());
     for(var i=0;i<1000;i++)
     {
         d=document.createElement('li');
         ul.appendChild(d);
-        d.innerHTML=i;
+        g=""+i;
         if(i%3==0 && i%5==0 )
         {
-            d.innerHTML="FizzBuzz";
+            g="FizzBuzz";
         }
         else if(i%5==0)
         {
-            d.innerHTML="Buzz";
+            g="Buzz";
         }
         else if(i%3==0 )
         {
-            d.innerHTML="Fizz";
+            g="Fizz";
         }
-
+        d.innerHTML=g;
     }
-
+    let f=new Date().getTime();
+    console.log(e);
+    console.log(f);
+    console.log(f-e);
 
 }
 //This is dom practice
-fizzbuzzchallenge();
-console.log("dbmcsdmhdmsbdckbds")
+
+
