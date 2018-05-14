40d16ebd905c4a170e62ba7d9a85a96b6b26a811
Vedaang Chopra <vedaangchopra@hotmail.com>
2018-05-14 21:45:06 +0530

    Nodejs code update

--- Practicing_NodeJs/f-05(Callbacks).js
+++ Practicing_NodeJs/f-05(Callbacks).js
@@ -2,17 +2,56 @@
 function one(a,onecb)
 {
     console.log(a);
-    onecb(a+1,(c,threecb)=>{
-        console.log(c);                             //In this callback methods we have definition and calls inside the function and
-        threecb(c+1,(e)=>{                          //within the function call of one.
+    onecb(a+1,function(c,threecb)
+    {
+        console.log(c);
+        threecb(c+1,(e)=>
+        {
             console.log(e);
-        });
-    });
+            console.log(a);
+            console.log(c);
+        })
+
+    })
 }
-one(1,(b,twocb)=>{
+one(1,function (b,twocb){
     console.log(b);
-    twocb(b+1,(d)=>{
+    twocb(b+1,function (d,fourcb){
         console.log(d);
         fourcb(d+1);
-    });
+    })
+
 });
+//Callback (example-2)...........................................................
+function downloadfile(url,downloaded){
+    setTimeout(()=>{
+        downloaded(null,"DATA DOWNLOADED");
+},2000);
+}
+function encrypt(data,encrypted)
+{
+    setTimeout(()=>{
+        encrypted(null,`{{{{{{{${data}}}}}}}}}`);
+    },1000);
+}
+function savedfile(data,saved)
+{
+    setTimeout(()=>{
+        saved(null,true)
+    },2000);
+}
+downloadfile("https://cb.lk/file.jpg",(err,data)=>{
+    if(!err){
+        encrypt(data,(err2,encData)=>{
+            if(!err2)
+            {
+                savedfile(encData,(err3,saved)=>{
+                    if(!err3 && saved)
+                    {
+                        console.log("download,encrypted and saved")
+                    }
+                })
+            }
+        })
+    }
+})
