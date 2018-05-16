996ff8350cdca60191dda58df82caf70af7c0419
Vedaang Chopra <vedaangchopra@hotmail.com>
2018-05-17 03:23:23 +0530

    Adding NodeJs Code

--- Practicing_NodeJs/f-10(Module for Import Export).js
+++ Practicing_NodeJs/f-10(Module for Import Export).js
@@ -1,3 +1,16 @@
 let a=10;
 let b=20;
-export const num=(a+b);
+export const num=(a+b);                                         //Here num and fun are called named exports.
+                                                                //Named exports need to be imported with the same name/.
+export const fun=function () {console.log(`Hello World`);
+}
+//This is called as default exports.They need to be imported in such a way that the all default variables exported are provided with a name.
+//We can not have more than default exports.
+export default {
+    p:20,
+    q:35
+}/*
+export default {
+    r:30,
+    x:40
+}*/
--- Practicing_NodeJs/f-10(New Inport Export Syntax).js
+++ Practicing_NodeJs/f-10(New Inport Export Syntax).js
@@ -1,3 +1,16 @@
-import {num} from './f-09(Module for new syntax Import Export.js)';     //File extensions are important for browser running as it is important
-                                                                        //because browser requests from server for exact file
+//File extensions are important for browser running as it is important because browser requests from server for exact file.
+//import {num} from './f-10(Module for Import Export).js';            //Importing Method 1 (same name variables).
+                                                                    //Num is the name of variable exported from library.
+import * as lib from './f-10(Module for Import Export).js';         //Importing method 2 (Import everything and use it as object).
+                                                                    //Here lib acts as an object and has every export of library to access.
+
+import x from './f-10(Module for Import Export).js';                 //Importing method 3 (Importing/Accessing default variables of library).
+                                                                     //In this manner x can only access default variables which were
+                                                                     //exported from library.
+import {num,default as y} from './f-10(Module for Import Export).js';   //Here in a single line everything is exported
+
+
 console.log(num);
+console.log(lib.fun());
+console.log(x.p+x.q);
+console.log(y.q+y.r+y.x);
